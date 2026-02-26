a8c752f02fea2a5d5d6d2bd03d294f1b725f87ef
Miguel <miguelvanuccisteam@gmail.com>
2026-02-26 10:06:05 -0300

    feat: adiciona métodos de comportamento

--- src/br/com/fiap/main/SistemaPrincipal.java
+++ src/br/com/fiap/main/SistemaPrincipal.java
@@ -6,29 +6,38 @@ public class SistemaPrincipal {
 
 	public static void main(String[] args) {
 
-		Mochila mochila1 = new Mochila();
+		Mochila mochila1 = new Mochila("Nylon", true, false);
 
-		mochila1.material = "Nylon";
 		mochila1.peso = 5;
 		mochila1.tamanho = 5;
 
-		Mochila mochila2 = new Mochila();
+		Mochila mochila2 = new Mochila("Couro", false, true);
 
-		mochila2.material = "Couro";
 		mochila2.peso = 3;
 		mochila2.tamanho = 2;
 
-		System.out.println("==== Mochilas ====");
-		System.out.println();
+		System.out.println("==== Mochilas ====\n");
 
 		System.out.println("Minha mochila é feita de " + mochila1.material);
 		System.out.println("Minha mochila pesa " + mochila1.peso + " Kilos");
-		System.out.println("Minha mochila tem " + mochila1.tamanho + " metros");
-
+		System.out.println("Minha mochila tem " + mochila1.tamanho + " metros\n");
+		
+		System.out.println("Abrindo mochila 1");
+		mochila1.abrirMochila(true);
+		System.out.println("Fechando mochila 1");
+		mochila1.fecharMochila(true);
+		
 		System.out.println();
-		System.out.println("Minha mochila é feita de " + mochila1.material);
-		System.out.println("Minha mochila pesa " + mochila1.peso + " Kilos");
-		System.out.println("Minha mochila tem " + mochila1.tamanho + " metros");
+		
+		System.out.println("Minha mochila é feita de " + mochila2.material);
+		System.out.println("Minha mochila pesa " + mochila2.peso + " Kilos");
+		System.out.println("Minha mochila tem " + mochila2.tamanho + " metros\n");
+		
+		System.out.println("Abrindo mochila 2");
+		mochila2.abrirMochila(false);
+		System.out.println("Fechando mochila 2");
+		mochila2.fecharMochila(false);
+		
 
 	}
 
--- src/br/com/fiap/model/Mochila.java
+++ src/br/com/fiap/model/Mochila.java
@@ -5,5 +5,38 @@ public class Mochila {
 	public int tamanho;
 	public String material;
 	public double peso;
+	public boolean aberto;
+	public boolean fechado;
 	
+	
+	public Mochila(String material, boolean aberto, boolean fechado) {
+		this.tamanho = 0;
+		this.material = material;
+		this.peso = 0;
+		this.aberto = aberto;
+		this.fechado = fechado;
+	}
+	
+	public void abrirMochila(boolean aberto) {
+		if (aberto) {
+			System.out.println("Abrindo mochila");
+			return;
+		}
+		if(aberto == false) {
+			System.out.println("Não abrindo mochila");
+			return;
+		}
+	}
+	
+	public void fecharMochila(boolean fechado) {
+		if (fechado) {
+			System.out.println("Fechando mochila");
+			return;
+		}
+		if(fechado == false) {
+				System.out.println("Não fechando mochila");
+				return;
+		}
+		
+	}
 }
