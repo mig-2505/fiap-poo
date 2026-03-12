3cbc8ef248985a9c908125523d696a2d983ffd30
Miguel <miguelvanuccisteam@gmail.com>
2026-03-12 08:09:43 -0300

    feat: adiciona métodos de comportamento no FiapRide

--- fiapRide/src/br/com/fiapride/main/SistemaPrincipal.java
+++ fiapRide/src/br/com/fiapride/main/SistemaPrincipal.java
@@ -5,17 +5,25 @@ import br.com.fiapride.model.Passageiro;
 public class SistemaPrincipal {
 
 	public static void main(String[] args) {
-		Passageiro passageiro1 = new Passageiro();
-		passageiro1.nome = "Ana Silva";
-		passageiro1.saldo = 50.0;
-		
-		Passageiro passageiro2 = new Passageiro();
-		passageiro2.nome = "Carlos Souza";
-		passageiro2.saldo = 12.50;
-		
-		System.out.println("--- Sistema FiapRide ---");
-		System.out.println("Passageiro: " + passageiro1.nome + " | Saldo: R$" + passageiro1.saldo);
-		
-		System.out.println("Passageiro: " + passageiro2.nome + " | Saldo: R$" + passageiro2.saldo);
-	}
+		Passageiro passageiro1 = new Passageiro("Ana Silva", "222");
+        System.out.println("Regarga passageiro 1");
+        passageiro1.adicionarSaldo(50.0);
+
+        
+        Passageiro passageiro2 = new Passageiro("Carlos Souza", "333");
+        System.out.println("Regarga passageiro 2");
+        passageiro2.adicionarSaldo(12.5);
+
+        System.out.println("--- Sistema FiapRide ---");
+        System.out.println("Passageiro: " + passageiro1.nome + " | Saldo: R$ " + passageiro1.saldo + " | CPF: " + passageiro1.cpf);
+
+        System.out.println("Passageiro: " + passageiro2.nome + " | Saldo: R$ " + passageiro2.saldo + " | CPF: " + passageiro2.cpf);
+
+
+        System.out.println("Pagando viagem do passageiro 1");
+        passageiro1.pagarViagem(20);
+        System.out.println("Pagando viagem do passageiro 2");
+        passageiro2.pagarViagem(20);
+
+        }
 }
